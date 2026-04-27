ee760178a2fcd8250111c4c1e91379ecd5f1c8a8
Forcella Mattia <mattia.forcella@stu.istitutoturoldo.edu.it>
2026-04-27 09:26:10 +0200

    Creato bottoni (solo 1 funzionante)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var BottoneOrario: Button
+    lateinit var BottonePTOF: Button
+    lateinit var BottoneRegistro: Button
+    lateinit var BottoneLibri: Button
+    lateinit var BottoneCircolari: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        BottoneOrario = findViewById<Button>(R.id.Orario)
+        BottonePTOF = findViewById<Button>(R.id.PTOF)
+        BottoneRegistro = findViewById<Button>(R.id.Registro)
+        BottoneLibri = findViewById<Button>(R.id.Libri)
+
+
+
+        BottoneOrario.setOnClickListener { v ->
+            val url = "https://www.istitutoturoldo.edu.it/didattica/calendario-scolastico/orario-scolastico/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+
     }
 }
