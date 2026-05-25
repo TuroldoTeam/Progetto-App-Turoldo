1a6232835a0ac9631a5d5e41f8073b9fe111bdef
Baldi <michele.baldi007@gmail.com>
2026-05-25 09:52:49 +0200

    Migliorato XML e Aggiunti 2 Pulsanti

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.net.Uri
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +16,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var bottonePTOF: Button
     lateinit var bottoneRegistro: Button
     lateinit var bottoneLibri: Button
-    lateinit var bottoneCircolari: Button
-    lateinit var url: String        // URL a cui puntano bottoni
+    lateinit var url: String            // URL a cui puntano bottoni
+
+    lateinit var bottoneTelefono: LinearLayout      // Telefono
+
+    lateinit var bottoneEmail: LinearLayout         // Email
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         bottonePTOF = findViewById<Button>(R.id.ptof)
         bottoneRegistro = findViewById<Button>(R.id.registro)
         bottoneLibri = findViewById<Button>(R.id.libri)
+        bottoneTelefono = findViewById<LinearLayout>(R.id.telefono)
+        bottoneEmail = findViewById<LinearLayout>(R.id.email)
 
 
 
@@ -58,6 +64,20 @@ class MainActivity : AppCompatActivity() {
 
         bottoneLibri.setOnClickListener { v ->
             url =  "https://www.istitutoturoldo.edu.it/didattica/adozioni-libri-di-testo/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        bottoneTelefono.setOnClickListener {
+            url = "tel:+39034592210"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        bottoneEmail.setOnClickListener {
+            url = "mailto:bgis013003@istruzione.it"
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(url)
             startActivity(intent)
